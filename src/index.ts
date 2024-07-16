3250c28e5c872a7b880189c5ff92bbe891ef5f1a
Sakai-Kaede <kana1024k@gmail.com>
2024-07-16 23:56:17 +0900

    Learned about variable declaration with const

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-const massage: string = 'Hello, World!';
+// ### constについて ###
 
+// constで宣言した変数の中身は、その後変わらない
+// そのため、letを使ったコードに比べて読む負担が減る
+// ## 目標 ## 9割り以上の変数をconstで宣言する
+const massage: string = 'Hello, World!';
 console.log(massage);
+
+// ## 注意 ## オブジェクトの中身を書き換えるのは、constに制限されない
+// 既存のオブジェクトの書き換えを行わないように意識しよう
+const arr = [1, 2, 3];
+arr[0] = 5;
+console.log(arr); // [ 5, 2, 3 ] と表示される
