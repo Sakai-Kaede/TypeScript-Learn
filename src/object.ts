1c80877d0cbe1a92c684a2eaf08168b24bc61280
Sakai-Kaede <kana1024k@gmail.com>
2024-07-26 23:56:21 +0900

    Learned about type checking

--- src/object.ts
+++ src/object.ts
@@ -1,4 +1,4 @@
-// スプレッド構文
+//// スプレッド構文 ////
 
 const obj1 = {
   foo: 123,
@@ -13,3 +13,17 @@ const obj2 = {
 
 // obj1がコピーされた後に、fooが上書きされる
 console.log(obj2);　// { foo: -9999, bar: 456, baz: 789 }と表示される
+
+
+//// オブジェクトの型 ////
+
+// objにカーソルを合わせると
+// const obj: {
+  // foo: number;
+  // bar: string;
+// }
+// と表示される（型チェック）
+const obj = {
+  foo: 123,
+  bar: 'Hello, World!'
+};
